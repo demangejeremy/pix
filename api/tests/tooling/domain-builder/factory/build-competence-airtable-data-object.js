deb6ca020bcf675936954a0c0d1516858a36b017
celineung <celine.ung@zenika.com>
2020-07-10 16:26:56 +0200

    Get translated competence info from Airtable

--- api/tests/tooling/domain-builder/factory/build-competence-airtable-data-object.js
+++ api/tests/tooling/domain-builder/factory/build-competence-airtable-data-object.js
@@ -1,9 +1,13 @@
 module.exports = function buildCompetenceAirtableDataObject({
   id = 'recsvLz0W2ShyfD63',
   name = 'Mener une recherche et une veille d’information',
+  nameFrFr = 'Mener une recherche et une veille d’information',
+  nameEnUs = 'Mener une recherche et une veille d’information',
   index = '1.1',
   areaId = 'recvoGdo7z2z7pXWa',
   description = 'Some description',
+  descriptionFrFr = 'Some description',
+  descriptionEnUs = 'Some description',
   origin = 'Pix',
   skillIds = [
     'recV11ibSCXvaUzZd',
@@ -21,10 +25,14 @@ module.exports = function buildCompetenceAirtableDataObject({
   return {
     id,
     name,
+    nameFrFr,
+    nameEnUs,
     index,
     areaId,
     origin,
     skillIds,
     description,
+    descriptionFrFr,
+    descriptionEnUs,
   };
 };
